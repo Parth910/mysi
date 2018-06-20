4ef82a346460f95ac10c6bc76d9921b11b656e95
parth <parthpatel4203>
2018-06-20 10:18:31 +0530

    heyhello

---
+++ script.js
@@ -0,0 +1,37 @@
+// var body=document.querySelector("body");
+// var content=document.querySelector("#content");
+// var none=false;
+// setInterval(function(){
+// 	if (none) {
+// 		body.style.background="#42FF33";
+// 		content.style.color="white";
+// 	}
+// 	else
+// 	{
+// 		body.style.background="white";
+// 		content.style.color="black";
+// 	}
+// 	none=!none;
+// },10);
+// var button=document.querySelector("button");
+// var bu=false;
+// button.addEventListener("click",function(){
+	
+// 	if(bu)
+// 	{button.style.background="blue";}
+// else
+// 	{button.style.background="default";}
+// 	bu=!bu;
+
+// });
+
+// var btns=document.querySelectorAll(".btn");
+
+// for(var i=0;i<btns.length;i++){
+//      btns[i].addEventListener("mouseover",function(){
+// //            alert("hovered");
+//             this.classList.add("bt");     });
+// }
+// var usrname=prompt("enter your name here!!");
+// alert("hello"+ usrname);
+// console.log(usrname);
